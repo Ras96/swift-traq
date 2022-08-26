e1ac05bbc964628e104214afbfc022a17c8e6a8c
Ras96 <asymptote.k.k@gmail.com>
2022-08-27 02:50:59 +0900

    :wrench: setup config file

--- update.sh
+++ update.sh
@@ -5,9 +5,7 @@ wget -nv -P /tmp https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v
 openapi-generator-cli generate \
   -g swift5 \
   -i /tmp/v3-api.yaml \
-  --additional-properties 'projectName=Traq' \
-  --additional-properties 'swiftUseApiNamespace=true' \
-  --additional-properties 'removeMigrationProjectNameClass=true'
+  -c ./.openapi-generator.yml
 
 # NOTE: enumの`-createdAt`と`createdAt`が共に`createdat`に変換されて衝突するため置換する
 MESSAGEAPI_FILE=./Traq/Classes/OpenAPIs/APIs/MessageAPI.swift
