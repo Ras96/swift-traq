bc17e6bf15b9830c87d6d9d5a66a8141e321fbd0
ras0q <ras0q@users.noreply.github.com>
2023-12-02 18:11:40 +0900

    :bug: fix generate.sh

--- generate.sh
+++ generate.sh
@@ -1,24 +1,14 @@
-#!/bin/sh -eux
-
-OPENAPI_GENERATOR_VERSION=$(cat .openapi-generator/VERSION)
+#!/bin/sh -eu
 
 # 前回ファイルを削除
 cat .openapi-generator/FILES | xargs rm -f
 
 # build
-docker run --rm -v "${PWD}:/local" -u $(id -u) openapitools/openapi-generator-cli:v$OPENAPI_GENERATOR_VERSION generate \
+docker run --rm -v "${PWD}:/local" -u $(id -u) openapitools/openapi-generator-cli:latest-release generate \
   -i https://raw.githubusercontent.com/traPtitech/traQ/master/docs/v3-api.yaml \
   -g swift5 \
   -c /local/.openapi-generator.yml \
   -o /local
-mv README.md client.md
-mv README.md.bak README.md
-
-# NOTE: enumの`-createdAt`と`createdAt`が共に`createdat`に変換されて衝突するため置換する
-MESSAGEAPI_FILE=./Sources/Traq/APIs/MessageAPI.swift
-sed -i.bak -e 's/createdat = "-createdAt"/createdatAsc = "-createdAt"/' $MESSAGEAPI_FILE
-sed -i.bak -e 's/updatedat = "-updatedAt"/updatedatAsc = "-updatedAt"/' $MESSAGEAPI_FILE
-rm ${MESSAGEAPI_FILE}.bak
 
 # SwiftFormatのルールを追加
 echo '--enumnamespaces structs-only' >> .swiftformat
